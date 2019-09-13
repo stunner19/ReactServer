d499b86f0a206370699360f31453d80adf5bb690
Karanpal Singh <singhkaranpal81@gmail.com>
2019-09-13 12:26:22 +0530

    Adding routes

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var mongoose = require('mongoose');
 var config = require('./config');
+var router = require('./router').default;
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
@@ -26,9 +25,7 @@ app.use(express.json());  // alternative for body-parser;
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+router(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
