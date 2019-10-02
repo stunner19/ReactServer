fae9d25848f9bc97ec7c2b2ef3e8b73802cfdfdf
Karanpal Singh <singhkaranpal81@gmail.com>
2019-10-02 11:51:16 +0530

    Authentication continued

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
+    'secretKey': 'sdfa8g87sf6d7s678asg7asd8f6av7df',
     'mongoUrl': 'mongodb://localhost:27017/auth'    // auth is the name of the database.
 }
--- models/user.js
+++ models/user.js
@@ -14,15 +14,23 @@ var User = new Schema({
 });
 
 // On Save Hook, encrypt password
+// Before saving the user, run this function.
 User.pre('save',(next) => {
+
+    // get access to user model.
     const user = this;
 
+    // generate a salt, then run callback.
     bcrypt.genSalt(10,(err,salt) => {
         if(err) { return next(err); }
+
+        // hash the password using the salt.
         bcrypt.hash(user.password,salt,null,(err,hash) => {
             if(err) { return next(err); }
+
+            // overwrite password with encrypted password.
             user.password = hash;
-            next();
+            next(); // save the model.
         })
     });
 });
